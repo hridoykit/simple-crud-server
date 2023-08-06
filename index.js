b7cf6b6bcd9f71c299efb365af8cf4eec5e06298
hridoykit <hridoy.tsa@gmail.com>
2023-08-07 03:35:04 +0600

    find multiple user data

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ async function run() {
 
     const userCol = client.db("usersDb").collection("users");
 
+    app.get('/users', async (req, res) => {
+      const cursor = userCol.find();
+      const result = await cursor.toArray();
+
+      res.send(result);
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await userCol.insertOne(user);
